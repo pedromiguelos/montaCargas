a827a62d0b831b337b9f5f3a24430b824498be01
TiagoSousa <tiagomichaelsousa@gmail.com>
2017-05-27 15:59:31 +0100

    Changed TileCellRender and Properties in Gui package

--- src/gui/Properties.java
+++ src/gui/Properties.java
@@ -1,19 +1,21 @@
 package gui;
 
 public class Properties {
-  public static final String IMAGE_DIR = "/images/";
-  public static final String IMAGE_PREFIXWALLHOR = "wallH";
-  public static final String IMAGE_PREFIXWALLVER = "wallV";
-  public static final String IMAGE_SUFFIX = ".png";
-  public static final String EMPTY_IMAGE = "Blank.JPG";
-  public static final String IMAGE_DOOR = "door.png";
-  public static final String IMAGE_FORKLIFT = "forklift.png";
-  
-  
-
-  public static final int CELL_WIDTH = 50;
-  public static final int CELL_HEIGHT = 50;
-
-  public static final int PUZZLE_WIDTH = 6;
-  public static final int PUZZLE_HEIGHT = 6;
+    public static final String IMAGE_DIR = "/images/";
+    public static final String IMAGE_SUFFIX = ".png";
+    public static final String IMAGE_PREFIXWALLH = "wallH";
+    public static final String IMAGE_PREFIXWALLV = "wallV";
+    
+    public static final String EMPTY_IMAGE = "Blank.JPG";
+    public static final String IMAGE_FORKLIFT = "forklift.png";
+    public static final String IMAGE_DOOR = "door.png";
+    
+    
+    
+    
+    public static final int CELL_WIDTH = 50;
+    public static final int CELL_HEIGHT = 50;
+    
+    public static final int PUZZLE_WIDTH = 6;
+    public static final int PUZZLE_HEIGHT = 6;
 }
--- src/gui/PuzzleTileCellRenderer.java
+++ src/gui/PuzzleTileCellRenderer.java
@@ -3,20 +3,19 @@ package gui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
-import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import utils.ImageLoader;
 
 public class PuzzleTileCellRenderer extends JLabel implements TableCellRenderer {
-
-    private ArrayList<Position> listaPending = new ArrayList<Position>();
-    private int forkListRow = -1;
+    
     public PuzzleTileCellRenderer() {
         setBackground(Color.WHITE);
         setOpaque(true);
+        setFont(new Font("Monospaced", Font.BOLD, 49));
         this.setHorizontalAlignment(SwingConstants.CENTER);
     }
+    
 
     public Component getTableCellRendererComponent(JTable table, Object value,
             boolean isSelected,
@@ -24,92 +23,42 @@ public class PuzzleTileCellRenderer extends JLabel implements TableCellRenderer 
             int column) {
         
         ImageLoader loader = ImageLoader.getLoader();
-        setText("");
-        
-        if(row == forkListRow && column == 5){
-            setIcon(loader.getIcon(Properties.IMAGE_DOOR));
-            return this;
-        }
-        int valor = ((Integer) value).intValue();
-        
-        //As pecas pares acima do 4, 6 e 8 as pecas estao na msm linha!
-        Position found = null;
-        for (Position position : listaPending) { //Percorre a lista das posicoes marcadas cujos blocos ainda nao terminaram
-            if(position.equals(row, column))
-                found = position;
-        }
-        if( found != null){
-            setIcon(loader.getIcon(found.path));
-            listaPending.remove(found);
-            
-            return this;
-        }
-        
-        switch(valor){
+
+        switch (((Integer) value).intValue()) {
             case 0:
                 setIcon(loader.getIcon(Properties.EMPTY_IMAGE));
                 break;
             case 1:
                 setIcon(loader.getIcon(Properties.IMAGE_FORKLIFT));
-                forkListRow = row;
+                if(column == 5)
+                     setIcon(loader.getIcon(Properties.IMAGE_DOOR));
                 break;
             case 2:
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLHOR + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLH + Properties.IMAGE_SUFFIX));
                 break;
             case 3:
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLVER + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLV + Properties.IMAGE_SUFFIX));
                 break;
             case 4:
-                listaPending.add(new Position(row, column+1, Properties.IMAGE_PREFIXWALLHOR + "2" + Properties.IMAGE_SUFFIX));//Add the position that hes waiting for
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLHOR + "2_1" + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLH + "2" + Properties.IMAGE_SUFFIX));
                 break;
             case 5:
-                listaPending.add(new Position(row+1, column, Properties.IMAGE_PREFIXWALLVER + "2" + Properties.IMAGE_SUFFIX));
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLVER + "2_1" + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLV + "2" + Properties.IMAGE_SUFFIX));
                 break;
             case 6:
-                listaPending.add(new Position(row, column+1, Properties.IMAGE_PREFIXWALLHOR + "3" + Properties.IMAGE_SUFFIX));
-                listaPending.add(new Position(row, column+2, Properties.IMAGE_PREFIXWALLHOR + "3" + Properties.IMAGE_SUFFIX));
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLHOR + "3_1" + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLH + "3" + Properties.IMAGE_SUFFIX));
                 break;
             case 7:
-                listaPending.add(new Position(row+1, column, Properties.IMAGE_PREFIXWALLVER + "3" + Properties.IMAGE_SUFFIX));
-                listaPending.add(new Position(row+2, column, Properties.IMAGE_PREFIXWALLVER + "3" + Properties.IMAGE_SUFFIX));
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLVER + "3_1" + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLV + "3" + Properties.IMAGE_SUFFIX));
                 break;
             case 8:
-                listaPending.add(new Position(row, column+1, Properties.IMAGE_PREFIXWALLHOR + "4" + Properties.IMAGE_SUFFIX));
-                listaPending.add(new Position(row, column+2, Properties.IMAGE_PREFIXWALLHOR + "4" + Properties.IMAGE_SUFFIX));
-                listaPending.add(new Position(row, column+3, Properties.IMAGE_PREFIXWALLHOR + "4" + Properties.IMAGE_SUFFIX));
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLHOR + "4_1" + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLH + "4" + Properties.IMAGE_SUFFIX));
                 break;
             case 9:
-                listaPending.add(new Position(row+1, column, Properties.IMAGE_PREFIXWALLVER + "4" + Properties.IMAGE_SUFFIX));
-                listaPending.add(new Position(row+2, column, Properties.IMAGE_PREFIXWALLVER + "4" + Properties.IMAGE_SUFFIX));
-                listaPending.add(new Position(row+3, column, Properties.IMAGE_PREFIXWALLVER + "4" + Properties.IMAGE_SUFFIX));
-                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLVER + "4_1" + Properties.IMAGE_SUFFIX));
+                setIcon(loader.getIcon(Properties.IMAGE_PREFIXWALLV + "4" + Properties.IMAGE_SUFFIX));
+                break;
         }
-        
+                
         return this;
-    }
-
-    private static class Position {
-        public int row;
-        public int column;
-        public String path;
-        public Position(int row, int column, String path) {
-            this.path = path;
-            this.row = row;
-            this.column = column;
-        }
-
-        public boolean equals(Position pos) {
-            return (pos.row == this.row && pos.column == this.column) ? true: false;
-        }
-        
-        public boolean equals(int row, int column) {
-            return (row == this.row && column == this.column) ? true: false;
-        }
-        
     }
 }
